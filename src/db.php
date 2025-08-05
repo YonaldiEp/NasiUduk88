fb9455cd20a4e20ca327b4939c44a2b51c0f08a3
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-08-05 11:45:35 +0700

    Setting Hosting

--- src/db.php
+++ src/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "nasiuduk88";
+$host = "sgp.domcloud.co";
+$user = "nasiuduk88";
+$pass = "825SaH3K-++sWoFzl9";
+$db   = "nasiuduk88_db";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
